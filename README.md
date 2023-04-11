c2ff5383176a8447cd468546934dd84c98c2db94
julia lima <eduarda.julia@alunoescolassociais.org.br>
2023-04-11 12:43:53 -0300

    Finzaliza projeto de calendário interativo

--- README.md
+++
@@ -1,2 +0,0 @@
-# calendario
-atividade programação web
---
+++ table.js
@@ -0,0 +1,8 @@
+function colorirData(){
+    let day            = document.getElementById('day').value;
+    let color          = document.getElementById('color').value;
+    let  calendar      = document.getElementById('calendar');
+    
+  let putColor         = calendar.getElementsByTagName('td')[parseInt(day)+5];
+  putColor.style.backgroundColor = color;
+}
